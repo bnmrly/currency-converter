41844343277d79673ef97bbc7b1330bbf59c0864
bnmrly <benjaminjohnmarley@gmail.com>
2026-06-01 16:08:03 +0100

    tweak theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { CurrencyForm } from "@/components/CurrencyForm/CurrencyForm";
 
 function App() {
   return (
-    <main className="border border-general-border m-4 rounded-md p-4 bg-app-surface">
+    <main className="m-4 rounded-md p-4 bg-app-surface">
       <header className="text-center pb-4">
         <h1 className="text-3xl font-semibold pb-2">Currency Converter</h1>
-        <p>Convert currencies instantly with live exchange rates</p>
+        <p className="text-accent">
+          Convert currencies instantly with live exchange rates
+        </p>
       </header>
       <CurrencyForm />
     </main>
